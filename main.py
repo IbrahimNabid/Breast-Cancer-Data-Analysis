92acf8e99a71deaaa873396fdec0b08dfa8ba46e
IbrahimNabid <79735610+IbrahimNabid@users.noreply.github.com>
2023-04-13 18:21:55 -0400

    import libraries

---
+++ main.py
@@ -0,0 +1,9 @@
+# import necessary libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+
